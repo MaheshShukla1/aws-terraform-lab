9504d0e718e441285178334a58b99de96ad76ad3
Mahesh Shukla - Aka JailBreaker 🚀 <shuklamahesh602@gmail.com>
2025-07-21 14:36:36 +0530

    Configure S3 bucket to block all public access

--- main.tf
+++ main.tf
@@ -25,3 +25,11 @@ resource "aws_s3_bucket_logging" "my_bucket_logging" {
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "log/"
 }
+
+resource "aws_s3_bucket_public_access_block" "my_bucket_block" {
+  bucket = aws_s3_bucket.log_bucket.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true 
+  restrict_public_buckets = true
+}
