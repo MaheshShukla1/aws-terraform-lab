37368f330baf7ff9569dddfbea78d63b1e42b5dd
Mahesh Shukla - Aka JailBreaker 🚀 <shuklamahesh602@gmail.com>
2025-07-21 12:33:03 +0530

    Enable S3 bucket versioning using aws_s3_bucket_versioning resource

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
   force_destroy = true
 }
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+ bucket = aws_s3_bucket.my_bucket.id
+
+ versioning_configuration {
+   status = "Enabled"
+ }
+}
