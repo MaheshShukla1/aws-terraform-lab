49dd349e5ea900dd9bb10a24153b743b735f2e94
Mahesh Shukla - Aka JailBreaker 🚀 <shuklamahesh602@gmail.com>
2025-07-21 12:09:51 +0530

    Updated s3 bucket config in variables

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "Aws region to create s3 bucket in"
+  type = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the s3 bucket"
+  type = string
+}
