84a499c0526a8593eaded601e069a37da18b683a
Mahesh Shukla - Aka JailBreaker 🚀 <shuklamahesh602@gmail.com>
2025-07-21 12:11:17 +0530

    Updated main with variables inputs

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "demo" {
-  bucket = "tf-demo-bucket-12345678"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  force_destroy = true
 }
