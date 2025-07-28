ec73c47b0dd2bde5fb044ec6917ac7874e6c63e3
Mahesh Shukla - Aka JailBreaker 🚀 <shuklamahesh602@gmail.com>
2025-07-28 15:30:36 +0530

    Added s3:ListBucket permissions to  bucket policy for practice-user

--- main.tf
+++ main.tf
@@ -33,3 +33,35 @@ resource "aws_s3_bucket_public_access_block" "my_bucket_block" {
   ignore_public_acls = true 
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "user_access_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowUserReadWriteAccess",
+        Effect    = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::851725195108:user/practice-user"  # ✅ Make sure this is exact ARN
+        },
+        Action    = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        Resource  = "${aws_s3_bucket.my_bucket.arn}/*",
+      },
+      {
+        Sid = "AllowUserListbucket",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::851725195108:user/practice-user"
+        },
+        Action = "s3:ListBucket",
+        Resource = aws_s3_bucket.my_bucket.arn
+      }
+    ]
+  })
+}
